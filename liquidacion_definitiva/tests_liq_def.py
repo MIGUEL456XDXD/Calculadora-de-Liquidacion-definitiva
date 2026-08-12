58ec76df0abd6153f4419fd2217fb14c0e3ecb2e
Hans Sch Otero <hsotero151@gmail.com>
2026-08-12 06:11:56 -0500

    Update tests_liq_def.py

--- liquidacion_definitiva/tests_liq_def.py
+++ liquidacion_definitiva/tests_liq_def.py
@@ -52,16 +52,68 @@ class TestsLiqDef ( unittest.TestCase):
         )
         
             # 3. VERIFICAR EL RESULTADO (Assert / Print)
-            print("--- RESULTADOS CASO NORMAL 2 (DIEGO GOMEZ) ---")
+            print("--- RESULTADOS CASO NORMAL 3 (DIEGO GOMEZ) ---")
                 
+            # Imprimimos el diccionario resultante línea por línea para que se vea ordenado
+            for llave, valor in resultado.items():
+                print(f"{llave}: ${valor:,.2f}")
+
+    def test_extraordinario_1(self):
+            
+            resultado = calcular_liquidacion_definitiva(
+            ingreso = date(2024, 5, 1),           # Año, Mes, Día
+            retiro = date(2026, 10, 31),          # Año, Mes, Día
+            sueldo_mensual = 1900000.0,           # Sin puntos ni comas
+            salario_total = 2149095.0,            # Sueldo + Auxilio
+            dias_pendientes = 0,
+            es_salario_integral = False
+        )
+            
+            # 3. VERIFICAR EL RESULTADO (Assert / Print)
+            print("--- RESULTADOS CASO EXTRAODINARIO 1 (NICOLAS OROZCO) ---")
+                    
+            # Imprimimos el diccionario resultante línea por línea para que se vea ordenado
+            for llave, valor in resultado.items():
+                print(f"{llave}: ${valor:,.2f}")
+
+    def test_extraordinario_2(self):
+            
+            resultado = calcular_liquidacion_definitiva(
+            ingreso = date(2026, 2, 1),           # Año, Mes, Día
+            retiro = date(2026, 2, 28),          # Año, Mes, Día
+            sueldo_mensual = 2000000.0,           # Sin puntos ni comas
+            salario_total = 2249095.0,            # Sueldo + Auxilio
+            dias_pendientes = 30,
+            es_salario_integral = False
+        )
+            
+            # 3. VERIFICAR EL RESULTADO (Assert / Print)
+            print("--- RESULTADOS CASO EXTRAODINARIO 2 (BRYAN MOSQUERA) ---")
+                    
+            # Imprimimos el diccionario resultante línea por línea para que se vea ordenado
+            for llave, valor in resultado.items():
+                print(f"{llave}: ${valor:,.2f}")
+
+    def test_extraordinario_3(self):
+            
+            resultado = calcular_liquidacion_definitiva(
+            ingreso = date(2025, 1, 1),           # Año, Mes, Día
+            retiro = date(2026, 7, 30),          # Año, Mes, Día
+            sueldo_mensual = 20000000.0,           # Sin puntos ni comas
+            salario_total = 20000000.0,            # Sueldo + Auxilio
+            dias_pendientes = 0,
+            es_salario_integral = True
+        )
+            
+            # 3. VERIFICAR EL RESULTADO (Assert / Print)
+            print("--- RESULTADOS CASO EXTRAODINARIO 3 (VALENTINA HIGUITA) ---")
+                    
             # Imprimimos el diccionario resultante línea por línea para que se vea ordenado
             for llave, valor in resultado.items():
                 print(f"{llave}: ${valor:,.2f}")
 
 
 
+
 if __name__ == '__main__':
     unittest.main()
-
-
-        
